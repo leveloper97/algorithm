1c1c699b261a026c15e006133fedcff4b3dd7d69
wonhee <wonhee3795@naver.com>
2020-01-10 11:10:29 +0900

    한글 유니코드 깨짐 수정

--- 16235.cpp
+++ 16235.cpp
@@ -1,7 +1,7 @@
 /*
 ** BOJ 16235
-** ���� ���� : �ùķ��̼� 
-** ���� ���� : ���� ����ũ
+** 문제 유형 : 시뮬레이션 
+** 문제 제목 : 나무 재테크
 ** https://www.acmicpc.net/problem/14501
 */
 
@@ -10,13 +10,13 @@
 
 using namespace std;
 
-const int MAX = 10; // n�� �ִ�� 10�̴�. 
-int map[MAX][MAX] = {0}; // ���� ����� ���� 
-int winter_food[MAX][MAX] = {0}; // �� �ܿ︶�� �߰��Ǵ� ��� 
-int n; // ������� ���� ��, �� 
-int m; // ������ ���� 
-int k; // k�� �� 
-int tree_add; // �߰��� ���� ���� 
+const int MAX = 10; // n의 최대는 10이다. 
+int map[MAX][MAX] = {0}; // 현재 양분의 상태 
+int winter_food[MAX][MAX] = {0}; // 매 겨울마다 추가되는 양분 
+int n; // 사재기한 땅의 행, 열 
+int m; // 나무의 개수 
+int k; // k년 후 
+int tree_add; // 추가된 나무 개수 
 int answer = 0;
 
 struct Tree {
@@ -25,7 +25,7 @@ struct Tree {
 	int year;
 	bool death = false; 
 };
-struct Tree tree[1000]; // 1000���� ����ü �迭�� �����Ѵ�. 
+struct Tree tree[1000]; // 1000개의 구조체 배열을 생성한다. 
 void do_spring();
 void do_summer();
 void do_fall();
@@ -35,33 +35,33 @@ void make_tree(int, int);
 int main() {
 	
 	scanf("%d%d%d", &n, &m, &k);
-	// ����� ����(5)�� �Է��Ѵ�. 
+	// 양분의 정보(5)를 입력한다. 
 	answer = m;
 	for (int y = 1; y <= n; y++) {
 		for (int x = 1; x <= n; x++) {
 			map[y][x] = 5;
 		}
 	}
-	// �ܿ︶�� �߰��� ����� �Է¹޴´�. 
+	// 겨울마다 추가할 양분을 입력받는다. 
 	for (int y = 1; y <= n; y++) {
 		for (int x = 1; x <= n; x++) {
 			scanf("%d", &winter_food[y][x]);
 		}
 	}
 	
-	// ���� ������ ������ �Է� �޴´�. 
-//	printf("���� ������ �Է¹޽��ϴ�. \n");
+	// 심은 나무의 정보를 입력 받는다. 
+//	printf("나무 정보를 입력받습니다. \n");
 	for (int i = 0; i < m; i++) {
 		scanf("%d%d%d", &tree[i].y, &tree[i].x, &tree[i].year);
 	}
-	// �������� ���̸� ���� �����Ѵ�. (���� ����) 
-//	printf("���̼����� �����մϴ�\n");
-	for (int i = 0; i < m; i++) { // i��°�� ã�´�. 
+	// 나무들을 나이를 토대로 정렬한다. (버블 정렬) 
+//	printf("나이순으로 정렬합니다\n");
+	for (int i = 0; i < m; i++) { // i번째를 찾는다. 
 		for (int j = 1; j < m - i; j++) {
 			int index;
-			// ���� �ִ� ���� �� ��� temp�� �ٲ۴�. 
+			// 옆에 있는 놈이 더 어리면 temp를 바꾼다. 
 			if (tree[j-1].year > tree[j].year) {
-//				printf("���� ���� �� ���\n");
+//				printf("옆에 놈이 더 어리다\n");
 				Tree temp = tree[j-1];
 				tree[j-1] = tree[j];
 				tree[j] = temp;
@@ -73,13 +73,13 @@ int main() {
 //		printf("%d%d%d", tree[i].y, tree[i].x, tree[i].year);
 //	}
 	
-	// k�⸸ŭ ������� ������. 
+	// k년만큼 사계절을 보낸다. 
 	for (int i = 0 ; i < k; i++) {
 		do_spring();
 		do_summer();
 		do_fall();
 		do_winter();	
-//		printf("%d���� ������ ���� %d\n", i+1, answer);
+//		printf("%d년후 나무의 개수 %d\n", i+1, answer);
 	}
 	
 	if (answer < 0) {
@@ -89,108 +89,108 @@ int main() {
 		
 	return 0;
 }
-// ��, �ڽ��� ���̸�ŭ ����� �԰� ���̸� �Դ´�. �ϳ��� ĭ�� �������� ������ ������ � �������� ����� �Դ´�. ����� �����ϸ� ����Ѵ�. 
+// 봄, 자신의 나이만큼 양분을 먹고 나이를 먹는다. 하나의 칸에 여러개의 나무가 있으면 어린 나무부터 양분을 먹는다. 양분이 부족하면 즉사한다. 
 void do_spring() {
-//	printf("���� �����մϴ�.\n");
+//	printf("봄을 시작합니다.\n");
 	for (int i = 0; i < m; i++) {
 		if (tree[i].death == true) {
-//			printf("���� �����Դϴ�. ��и� ���� �ʽ��ϴ�.\n");
+//			printf("죽은 나무입니다. 양분를 먹지 않습니다.\n");
 			break; 
 		}
-		int food = map[tree[i].y][tree[i].x]; // ���� ������ �ִ� ���� ����� ��
-//	printf("���� ���� ��� : %d\n", food);
+		int food = map[tree[i].y][tree[i].x]; // 현재 나무가 있는 곳의 양분의 양
+//	printf("현재 남은 양분 : %d\n", food);
 		if (food - tree[i].year < 0) {
-//			printf("������ �׾����ϴ�.\n");
+//			printf("나무가 죽었습니다.\n");
 			answer--;
 			tree[i].death = true;
 		} else {
-//			printf("����� �Ծ����ϴ�. \n");
-			food -= tree[i].year; // ������ ���̸�ŭ ����� ó��
-			tree[i].year++; // ���� �ѻ� ó�� 	
+//			printf("양분을 먹었습니다. \n");
+			food -= tree[i].year; // 나무의 나이만큼 양분을 처묵
+			tree[i].year++; // 나이 한살 처묵 	
 		}
-		map[tree[i].y][tree[i].x] = food; // ���� ��� �ʿ� ������Ʈ 
+		map[tree[i].y][tree[i].x] = food; // 남은 양분 맵에 업데이트 
 	}
 }
-// ����, ���� ������ ������� ���Ѵ�. ���� ������ ���̸� 2�� ���� ���� ����� ���Ѵ�. (�Ҽ����� ������.) 
+// 여름, 죽은 나무가 양분으로 변한다. 죽은 나무의 나이를 2로 나눈 값을 양분을 더한다. (소수점은 버린다.) 
 void do_summer() {
-//	printf("������ �����մϴ�.\n");
+//	printf("여름을 시작합니다.\n");
 	for (int i = 0; i < m; i++) {
 		if (tree[i].death == true) {
-//			printf("���� ������ ������� �ٲߴϴ�.\n");
+//			printf("죽은 나무를 양분으로 바꿉니다.\n");
 			int food = tree[i].year / 2;
 			map[tree[i].y][tree[i].x] += food;
 		}
 	}
 }
-// ����, ������ ���̰� 5�� ����̸� ������ 8���� ĭ�� �����Ѵ�. ���� ����� ������ ������ �ʴ´�. 
+// 가을, 나무의 나이가 5의 배수이면 인접한 8개의 칸에 번식한다. 땅을 벗어나면 나무가 생기지 않는다. 
 void do_fall() {
-//	printf("������ �����մϴ�.\n");
+//	printf("가을을 시작합니다.\n");
 	tree_add = 0;
 	for (int i = 0; i < m; i++) {
 		if (tree[i].year % 5 == 0 && tree[i].death == false) {
-//			printf("������ ���̰� 5�� ����̴�. �����Ѵ�\n");
+//			printf("나무의 나이가 5의 배수이다. 번식한다\n");
 			int y = tree[i].y;
 			int x = tree[i].x;
-//			printf("������ �༮�� y ��ǥ %d\n", y);
-//			printf("������ �༮�� x ��ǥ %d\n", x);
+//			printf("번식할 녀석의 y 좌표 %d\n", y);
+//			printf("번식할 녀석의 x 좌표 %d\n", x);
 			if (0 < y-1 && y-1 < n + 1 && 0 < x-1 && x-1 < n + 1) {
-//				printf("���� ���� %d%d\n", y-1, x-1);
+//				printf("번식 성공 %d%d\n", y-1, x-1);
 				tree_add++;
 				make_tree(y-1, x-1);	
 			}
 			if (0 < y-1 && y-1 < n + 1 && 0 < x && x < n + 1) {
-//				printf("���� ���� %d%d\n", y-1, x);
+//				printf("번식 성공 %d%d\n", y-1, x);
 				tree_add++;
 				make_tree(y-1, x);	
 			}
 			if (0 < y-1 && y-1< n + 1 && 0 < x+1 && x+1< n + 1) {
-//				printf("���� ���� %d%d\n", y-1, x+1);
+//				printf("번식 성공 %d%d\n", y-1, x+1);
 				tree_add++;
 				make_tree(y-1, x+1);	
 				
 			}
 			if (0 < y && y < n + 1 && 0 < x-1 && x-1 < n + 1) {
-//				printf("���� ���� %d%d\n", y, x-1);
+//				printf("번식 성공 %d%d\n", y, x-1);
 				tree_add++;
 				make_tree(y, x-1);	
 				
 			}
 			if (0 < y && y < n + 1 && 0 < x+1 && x+1 < n + 1) {
-//				printf("���� ���� %d%d\n", y, x+1);
+//				printf("번식 성공 %d%d\n", y, x+1);
 				tree_add++;
 				make_tree(y, x+1);	
 				
 			}
 			if (0 < y+1 && y+1 < n + 1 && 0 < x-1 && x-1 < n + 1) {
-//				printf("���� ���� %d%d\n", y+1, x-1);
+//				printf("번식 성공 %d%d\n", y+1, x-1);
 				tree_add++;
 				make_tree(y+1, x-1);	
 				
 			}
 			if (0 < y+1 && y+1 < n + 1 && 0 < x && x < n + 1) {
-//				printf("���� ���� %d%d\n", y+1, x);
+//				printf("번식 성공 %d%d\n", y+1, x);
 				tree_add++;
 				make_tree(y+1, x);	
 				
 			}
 			if (0 < y+1 && y+1 < n + 1 && 0 < x+1 && x+1< n + 1) {
-//				printf("���� ���� %d%d\n", y+1, x+1);
+//				printf("번식 성공 %d%d\n", y+1, x+1);
 				tree_add++;
 				make_tree(y+1, x+1);		
 			} else {
-//				printf("���� �������� : %d%d", y+1, x+1);
+//				printf("번식 실패이유 : %d%d", y+1, x+1);
 			}
 			 
 		}
 	}
 	m += tree_add;
-	// ������ ������� ���̼� ������ �Ѵ�. 
-	for (int i = 0; i < m; i++) { // i��°�� ã�´�. 
+	// 번식한 기념으로 나이순 정렬을 한다. 
+	for (int i = 0; i < m; i++) { // i번째를 찾는다. 
 		for (int j = 1; j < m - i; j++) {
 			int index;
-			// ���� �ִ� ���� �� ��� temp�� �ٲ۴�. 
+			// 옆에 있는 놈이 더 어리면 temp를 바꾼다. 
 			if (tree[j-1].year > tree[j].year) {
-//				/printf("���� ���� �� ���\n");
+//				/printf("옆에 놈이 더 어리다\n");
 				Tree temp = tree[j-1];
 				tree[j-1] = tree[j];
 				tree[j] = temp;
@@ -198,18 +198,18 @@ void do_fall() {
 		}
 	}
 }
-// �ܿ�, �κ��� ���� ���ƴٴϸ鼭 ����� �߰��Ѵ�. �Է����� �־���.. 
+// 겨울, 로봇이 땅을 돌아다니면서 양분을 추가한다. 입력으로 주어짐.. 
 void do_winter() {
-//	printf("�ܿ��� �����մϴ�.\n");
+//	printf("겨울을 시작합니다.\n");
 	for (int y = 1; y <= n; y++) {
 		for (int x = 1; x <= n; x++) {
 			map[y][x] += winter_food[y][x];
 		}
 	}
 }
-// ���ڿ��� ������ 8���� ĭ�� ���̰� 1�� ������ �����Ѵ�. 
+// 인자에서 인접한 8개의 칸에 나이가 1인 나무를 생성한다. 
 void make_tree(int y, int x) {
-	// ���� �߰� 
+	// 나무 추가 
 	tree[m -1 + tree_add].y = y;
 	tree[m -1 + tree_add].x = x;
 	tree[m -1 + tree_add].year = 1;
@@ -218,9 +218,9 @@ void make_tree(int y, int x) {
 	answer++;	
 }
 /*
-�ٽû����غ���
-1. �Ϲ����� ������ �迭�� �ε����� 0,0���� �����ϴµ�,
-�̰��� 1���� �����ϱ� ������ �̷��� ó���� ���� ������� �־���.
-2. ����������� ������Ƽ���� ���� ����ü�� ���� �Ǿ���.  
+다시생각해보기
+1. 일반적인 이차원 배열의 인덱싱은 0,0부터 시작하는데,
+이경우는 1부터 시작하기 때문에 이러한 처리에 많은 어려움이 있엇다.
+2. 관리해줘야할 프로퍼티들이 많아 구조체를 쓰게 되었다.  
  
 */
